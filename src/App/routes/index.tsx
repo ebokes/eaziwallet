bf77e438d0f96bb3538196c32d3e4ba44e73861d
Chibuokem Egbuchulam <talk2chibu@gmail.com>
2025-12-23 17:11:27 +0100

    feat: create auth pages

---
+++ src/App/routes/index.tsx
@@ -0,0 +1,19 @@
+// src/app/routes/index.tsx
+import { lazy } from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+
+const Splash = lazy(() => import("../../features/auth/pages/Splash"));
+const Login = lazy(() => import("../../features/auth/pages/Login"));
+
+export default function AppRoutes() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        {/* Public routes */}
+        <Route path="/" element={<Splash />} />
+        <Route path="/login" element={<Login />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
